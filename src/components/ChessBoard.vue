6ea15f3d5f18167c17712ba8f4871c13fa305d8f
Atomic71 <tomicandrej71@gmail.com>
2023-12-16 12:16:42 +0100

    chore: import types and add perspective to props

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,37 +1,37 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
+import type { ChessFile, ChessRank, Perspective } from '@/types';
+const props = defineProps<{
+  perspective: Perspective;
+}>();
 
-const perspective = ref<'black' | 'white'>('white');
-
-const files = ref(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
-const ranks = ref([1, 2, 3, 4, 5, 6, 7, 8]);
-
-const chessBoard = ref<HTMLDivElement>();
+const files = ref<ChessFile[]>(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
+const ranks = ref<ChessRank[]>(['1', '2', '3', '4', '5', '6', '7', '8']);
 
 const orderedFiles = computed(() => {
-  return perspective.value === 'white'
+  return props.perspective === 'white'
     ? files.value
     : files.value.slice().reverse();
 });
 
 const orderedRanks = computed(() => {
-  return perspective.value === 'white'
+  return props.perspective === 'white'
     ? ranks.value
     : ranks.value.slice().reverse();
 });
 </script>
 
 <template>
-  <h1>Chessboard</h1>
-  <button
+  <!-- <h1>Chessboard</h1> -->
+  <!-- <button
     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     @click="perspective = perspective === 'white' ? 'black' : 'white'"
   >
     toggle
   </button>
-  <p>watching as {{ perspective }}</p>
-  <div class="border-black justify-center flex">
-    <div class="p-5 bg-orange-300">
+  <p>watching as {{ perspective }}</p> -->
+  <div class="border-black flex">
+    <div class="p-5 bg-orange-800">
       <div class="flex border-2 border-black">
         <div
           v-for="(file, i) in orderedFiles"
@@ -40,13 +40,16 @@ const orderedRanks = computed(() => {
           <div
             v-for="rank in orderedRanks"
             :key="rank"
-            class="w-[6vw] h-[6vw]"
+            class="w-[10vw] h-[10vw] max-h-[10vh] max-w-[10vh]"
             :class="[
-              (i + rank) % 2 === 0 ? 'bg-white' : 'bg-black',
-              'flex justify-center items-center text-2xl text-red-600',
+              (i + Number(rank)) % 2 === 0 ? 'bg-gray-200' : 'bg-green-900',
+              'flex justify-center items-center text-2xl',
             ]"
           >
-            <div class=""></div>
+            <slot
+              name="square"
+              v-bind="{ file, rank }"
+            />
           </div>
         </div>
       </div>
