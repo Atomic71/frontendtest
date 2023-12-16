f93aa0ad19c9f1a91363b7ceb3a43a0288a83a19
Atomic71 <tomicandrej71@gmail.com>
2023-12-16 14:00:18 +0100

    chore: add isSquareDark fn. and delete isHighlighted

--- src/utils.ts
+++ src/utils.ts
@@ -11,11 +11,6 @@ export const getPosition = (
   rank: ChessRank
 ): ChessSquarePosition => `${file}${rank}`;
 
-export const isHighlighted = (
-  square: ChessSquarePosition,
-  highlightedPositions: ChessSquarePosition[]
-): boolean => highlightedPositions.includes(square);
-
 export const addEntryToRegistry = (
   position: ChessSquarePosition,
   registry: SquareClickedRegistry
@@ -27,3 +22,18 @@ export const addEntryToRegistry = (
     formattedTimestamp: dayjs().format('HH:mm:ss'),
   },
 ];
+
+export const isSquareDark = (file: ChessFile, rank: ChessRank): boolean => {
+  // a = 97, b = 98, etc.
+  const asciiBaseForFiles = 'a'.charCodeAt(0);
+
+  const asciiFile = file.charCodeAt(0);
+
+  const fileIndex = asciiFile - asciiBaseForFiles; // get 0-indexed file, a = 0, b = 1, etc
+
+  // Check if combined position is even;
+  const isCombinedPositionEven = (fileIndex + Number(rank)) % 2 === 0;
+
+  // Even positions are dark squares
+  return isCombinedPositionEven;
+};
