8c0c3cf9a7d2a9ae545eb239669906c4a9e5e8ca
Atomic71 <tomicandrej71@gmail.com>
2023-12-17 12:51:47 +0100

    fix: pass ref value to hook

--- src/hooks/useBoardRenderOrder.ts
+++ src/hooks/useBoardRenderOrder.ts
@@ -1,18 +1,19 @@
-import { computed, ref } from 'vue';
+import { Ref, computed, ref } from 'vue';
 import { ChessFile, ChessRank, Perspective } from '../types';
 
-export const useBoardRenderOrder = (perspective: Perspective) => {
+export const useBoardRenderOrder = (perspective: Ref<Perspective>) => {
+  console.log('useBoardRenderOrder', perspective);
   const ranks = ref<ChessRank[]>(['1', '2', '3', '4', '5', '6', '7', '8']);
   const files = ref<ChessFile[]>(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
 
   const fileRenderOrder = computed(() => {
-    return perspective === 'white'
+    return perspective.value === 'black'
       ? files.value
       : files.value.slice().reverse();
   });
 
   const rankRenderOrder = computed(() => {
-    return perspective === 'black'
+    return perspective.value === 'black'
       ? ranks.value
       : ranks.value.slice().reverse();
   });
