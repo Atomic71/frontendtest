5f20a69b23c54da8acfe6a5a5b30e9c4fe08e530
Atomic71 <tomicandrej71@gmail.com>
2023-12-17 14:43:03 +0100

    chore: add ChessSquare type

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 export type ChessFile = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
 export type ChessRank = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
 
-export type ChessSquarePosition = `${ChessFile}${ChessRank}`;
-
-export type SquareClickEvent = {
+export type ChessSquare = {
   file: ChessFile;
   rank: ChessRank;
 };
+
+export type ChessSquarePosition = `${ChessFile}${ChessRank}`;
 
 export type Perspective = 'white' | 'black';
 
