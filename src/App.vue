e0209613c46547d50e9c66d8d2611023ec05141d
Atomic71 <tomicandrej71@gmail.com>
2023-12-17 14:55:01 +0100

    chore: change chessboard rendering

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script lang="ts" setup>
-import Playground from './components/Playground.vue';
+import ChessBoard from '@/components/ChessBoard.vue';
+import Sidebar from '@/components/Sidebar.vue';
+import { useClickRegistry } from '@/hooks';
+import { Perspective } from '@/types';
+import { ref } from 'vue';
+
+const { addToRegistry, squareClickedRegistry } = useClickRegistry();
+
+const perspective = ref<Perspective>('white');
 </script>
 <template>
   <section
-    class="px-4 bg-slate-300 max-h-screen overflow-hidden h-screen flex flex-col pb-3"
+    class="p-4 bg-slate-300 max-h-screen overflow-hidden h-screen flex flex-col md:flex-row pb-3"
   >
-    <h1>Chess.com frontend test</h1>
-    <Playground />
+    <div class="flex-1">
+      <ChessBoard
+        :perspective="perspective"
+        @square-clicked="addToRegistry"
+      />
+    </div>
+
+    <div class="overflow-hidden">
+      <Sidebar
+        :registry="squareClickedRegistry"
+        :perspective="perspective"
+        @change-perspective="perspective = $event"
+      />
+    </div>
   </section>
 </template>
