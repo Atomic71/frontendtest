e6b59101a1d80ca3d22e1fd6754833932e025b07
Atomic71 <tomicandrej71@gmail.com>
2023-12-17 12:22:44 +0100

    chore: display playground in the app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
-import ChessBoard from './components/ChessBoard.vue';
-import Sidebar from './components/Sidebar.vue';
+import Playground from './components/Playground.vue';
 </script>
 <template>
-  <ChessBoard />
-  <Sidebar />
+  <section
+    class="px-4 bg-slate-300 max-h-screen overflow-hidden h-screen flex flex-col pb-3"
+  >
+    <h1>Chess.com frontend test</h1>
+    <Playground />
+  </section>
 </template>
