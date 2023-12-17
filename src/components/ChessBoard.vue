74c95635db92194592dc6dcfa717b3ca1332e892
Atomic71 <tomicandrej71@gmail.com>
2023-12-17 14:53:53 +0100

    chore: use chessquare component

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-import type { Perspective } from '@/types';
-import { useBoardRenderOrder } from '@/hooks';
+import type { Perspective, ChessSquare } from '@/types';
+import { useBoardRenderOrder, useHighlightedSquares } from '@/hooks';
 import { toRef } from 'vue';
+import ChessBoardSquare from './ChessBoardSquare.vue';
 
 const props = defineProps<{
   perspective: Perspective;
 }>();
 
-const perspective = toRef(props, 'perspective');
+const emit = defineEmits<{
+  (e: 'square-clicked', square: ChessSquare): void;
+}>();
 
+const perspective = toRef(props, 'perspective');
 const { finalOrder } = useBoardRenderOrder(perspective);
+const { handleHighlightSquare, isHighlighted } = useHighlightedSquares();
+
+const handleOnClick = (square: ChessSquare) => {
+  handleHighlightSquare(square);
+  emit('square-clicked', square);
+};
 </script>
 
 <template>
@@ -18,14 +28,15 @@ const { finalOrder } = useBoardRenderOrder(perspective);
   >
     <div
       v-for="square in finalOrder"
+      @click="handleOnClick(square)"
       :key="Object.values(square).join('')"
       :class="['aspect-square relative']"
     >
-      <slot
-        name="square"
-        v-bind="{
-          square,
-        }"
+      <ChessBoardSquare
+        :square="square"
+        :is-highlighted="isHighlighted(square)"
+        :show-rank="square.file === (perspective === 'white' ? 'a' : 'h')"
+        :show-file="square.rank === (perspective === 'white' ? '1' : '8')"
       />
     </div>
   </div>
